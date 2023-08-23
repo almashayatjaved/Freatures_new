2206d25d8c58cb800c9eb3c4af916f4a10317fa7
almashayatjaved <radiantdev03@gmail.com>
2023-08-23 18:00:02 +0500

    adding backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1657476",
+  key: "c6bc1ac2b4c5317a999a",
+  secret: "ff28b57939cfb274f705",
+  cluster: "ap3",
+  useTLS: true
+});
+
+
+const app = express();
+
+app.use(cors({
+    origin:['http://localhost:8000']
+}))
+
+
+app.use(express.json())
+
+app.post('/api/messages', async()=>{
+   await pusher.trigger("chatbox", "message", {
+        username: req.body.username,
+        message: req.body.message
+      });
+      
+      return res.json([]);
+})
+
+console.log('Listening to port 8000')
+
+app.listen(8000)
